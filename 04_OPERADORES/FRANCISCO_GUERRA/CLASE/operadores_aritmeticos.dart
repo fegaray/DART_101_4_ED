203f5d7800c5878527991e2569718ae7f05ede2d
fegaray <fegaray_2687@hotmail.com>
2024-11-11 10:01:14 -0500

    ActulizarOperacionesAritmeticas

---
+++ 04_OPERADORES/FRANCISCO_GUERRA/CLASE/operadores_aritmeticos.dart
@@ -0,0 +1,17 @@
+void main (){
+  final v1 = 10;
+  final v2 = 15;
+  final Suma = v1 + v2 ;
+  print (Suma);
+
+  final String1 = "Francisco";
+  final String2 = "Guerra";
+  final Mensaje = String1 + " " + String2 ;
+  print(Mensaje);
+  final Mensaje2 = "$String1+ "+"+ $String2";
+  print(Mensaje2);
+  print(Mensaje2.length);
+  print(Mensaje2.substring(1));
+  print(Mensaje2.substring(1,13));
+  print("Hola");
+}
